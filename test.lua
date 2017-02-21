a31911621f19b8ee4a5a583f1ff4f543e989bec0
Ben Agricola <bagricola@squiz.co.uk>
2017-02-21 17:47:09 +0000

    Add test benchmarks emulating golibs/spacesaving methods
    Signed-off-by: Ben Agricola <bagricola@squiz.co.uk>

--- test.lua
+++ test.lua
@@ -1,22 +1,61 @@
 local ffi = require("ffi")
-local math = require("math")
-local rnd = math.random
-local C = ffi.C
-local SpaceSaving = require("spacesaving")
+C = ffi.load("rt", true)
+
+
+ffi.cdef[[
+    typedef long time_t;
+    typedef int clockid_t;
+
+    typedef struct timespec {
+            time_t   tv_sec;        /* seconds */
+            long     tv_nsec;       /* nanoseconds */
+    } nanotime;
+
+    int clock_gettime(clockid_t clk_id, struct timespec *tp);
+]]
 
 local function get_time()
-    return tonumber(C.get_time_ns())
+    local pnano = assert(ffi.new("nanotime[?]", 1))
+
+    -- CLOCK_REALTIME -> 0
+    ffi.C.clock_gettime(0, pnano)
+    return tonumber(pnano[0].tv_sec) * 1e9 + tonumber(pnano[0].tv_nsec)
 end
 
-local t1 = SpaceSaving:new(256, 5)
-local cumulative = 0
-local iterations = 1000000
-for i = 1, iterations, 1 do
-    local t1 = get_time()
-    t1:touch(i, math.random(100))
-    cumulative = cumulative + (get_time() - t1)
+local math = require("math")
+local rnd = math.random
+local SpaceSaving = require("spacesaving")
+
+local function benchmark(num, iterations, hitrate)
+    local cumulative = 0
+    local ss = SpaceSaving:new(num, 1)
+
+    -- Warmup
+    for i = 1, num, 1 do
+        ss:touch(tostring(i), get_time())
+    end
+ 
+    local topRange = math.floor(num * 1 / hitrate)
+
+    for i = 1, iterations, 1 do
+        local key = tostring(rnd(topRange))
+        local t1 = get_time()
+        ss:touch(key, get_time())
+        cumulative = cumulative + (get_time() - t1)
+    end
+    print(num .. " Buckets / " .. iterations .. " Iterations / " .. hitrate .. " Hit Rate -- NS Per Operation: " .. cumulative / iterations)
 end
 
-print("NS Per Operation: " .. cumulative / iterations)
-
-
+local iters = 5000000
+benchmark(16384, 1e6, 0.1)
+benchmark(37268, 1e6, 0.1)
+benchmark(16384, 1e6, 0.5)
+benchmark(37268, 1e6, 0.5)
+benchmark(16384, 2e6, 0.9)
+benchmark(37268, 2e6, 0.9)
+benchmark(1024, 5e6, 1)
+benchmark(2048, 3e6, 1)
+benchmark(4096, 3e6, 1)
+benchmark(8192, 3e6, 1)
+benchmark(16384, 3e6, 1)
+benchmark(32768, 3e6, 1)
